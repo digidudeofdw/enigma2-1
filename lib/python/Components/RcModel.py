e213aae9a93cff11df8416fbe5d0a413e8c9c85a
fermi <perpect7@dags.kr>
2013-03-11 12:29:02 +0900

    1. remocon

--- lib/python/Components/RcModel.py
+++ lib/python/Components/RcModel.py
@@ -2,13 +2,13 @@ import os
 
 class RcModel:
 	RCTYPE_DMM = 0
-	RCTYPE_ET9X00 = 1
-	RCTYPE_ET6X00 = 2
-	RCTYPE_ET9500 = 3
-	RCTYPE_VU = 4
-	RCTYPE_ET4X00 = 5
-	RCTYPE_XP1000 = 6
-	RCTYPE_TMTWIN = 7
+	RCTYPE_IOS100 = 1
+	RCTYPE_IOS200 = 2
+	RCTYPE_IOS300 = 3
+	RCTYPE_TMTWIN = 4
+	RCTYPE_TM2T = 5
+	RCTYPE_TMSINGLE = 6
+	RCTYPE_TMNANO = 7
 
 	def __init__(self):
 		self.currentRcType = self.RCTYPE_DMM
@@ -24,49 +24,48 @@ class RcModel:
 		out = fp.read()
 		fp.close()
 		return out.split()[0]
-
+#[iq
 	def readRcTypeFromProc(self):
 		if os.path.exists('/proc/stb/info/hwmodel'):
 			model = self.readFile('/proc/stb/info/hwmodel')
 			if model == "tmtwinoe":
 				self.currentRcType = self.RCTYPE_TMTWIN
+			elif model == "ios100hd":
+				self.currentRcType = self.RCTYPE_IOS100
+			elif model == "ios200hd":
+				self.currentRcType = self.RCTYPE_IOS200
+			elif model == "ios300hd":
+				self.currentRcType = self.RCTYPE_IOS300
+			elif model == "tm2toe":
+				self.currentRcType = self.RCTYPE_TM2T
+			elif model == "tmsingleoe":
+				self.currentRcType = self.RCTYPE_TMSINGLE
+			elif model == "tmsingleoe":
+				self.currentRcType = self.RCTYPE_TMNANO
+
 		elif os.path.exists('/proc/stb/info/boxtype'):
 			model = self.readFile('/proc/stb/info/boxtype')
 			if model.startswith('et') or model.startswith('xp'):
 				rc = self.readFile('/proc/stb/ir/rc/type')
 				if rc == '4':
 					self.currentRcType = self.RCTYPE_DMM
-				elif rc == '5' or rc == '11':
-					self.currentRcType = self.RCTYPE_ET9X00
 				elif rc == '6':
 					self.currentRcType = self.RCTYPE_DMM
-				elif rc == '7':
-					self.currentRcType = self.RCTYPE_ET6X00
-				elif rc == '8':
-					self.currentRcType = self.RCTYPE_VU
-				elif rc == '9':
-					self.currentRcType = self.RCTYPE_ET9500
-				elif rc == '13':
-					self.currentRcType = self.RCTYPE_ET4X00
-				elif rc == '14':
-					self.currentRcType = self.RCTYPE_XP1000
-		elif os.path.exists('/proc/stb/info/vumodel'):
-			self.currentRcType = self.RCTYPE_VU
 
 	def getRcLocation(self):
-		if self.currentRcType == self.RCTYPE_ET9X00:
-			return '/usr/share/enigma2/rc_models/et9x00/'
-		elif self.currentRcType == self.RCTYPE_ET9500:
-			return '/usr/share/enigma2/rc_models/et9500/'
-		elif self.currentRcType == self.RCTYPE_ET6X00:
-			return '/usr/share/enigma2/rc_models/et6x00/'
-		elif self.currentRcType == self.RCTYPE_ET4X00:
-			return '/usr/share/enigma2/rc_models/et4x00/'
-		elif self.currentRcType == self.RCTYPE_XP1000:
-			return '/usr/share/enigma2/rc_models/xp1000/'
-		elif self.currentRcType == self.RCTYPE_TMTWIN:
+		if self.currentRcType == self.RCTYPE_TMTWIN:
 			return '/usr/share/enigma2/rc_models/tm/tmtwinoe/'
-		elif self.currentRcType == self.RCTYPE_VU:
-			return '/usr/share/enigma2/rc_models/vu/'
+		elif self.currentRcType == self.RCTYPE_IOS100:
+			return '/usr/share/enigma2/rc_models/tm/ios100/'
+		elif self.currentRcType == self.RCTYPE_IOS200:
+			return '/usr/share/enigma2/rc_models/tm/ios200/'
+		elif self.currentRcType == self.RCTYPE_IOS300:
+			return '/usr/share/enigma2/rc_models/tm/ios300/'
+		elif self.currentRcType == self.RCTYPE_TM2T:
+			return '/usr/share/enigma2/rc_models/tm/tm2toe/'
+		elif self.currentRcType == self.RCTYPE_TMSINGLE:
+			return '/usr/share/enigma2/rc_models/tm/tmsingleoe/'
+		elif self.currentRcType == self.RCTYPE_TMNANO:
+			return '/usr/share/enigma2/rc_models/tm/tmnanooe/'
 
 rc_model = RcModel()
