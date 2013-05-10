ae002a989902e5c819afa9d8d2767bb395f3b5b2
fermi <perpect7@dags.kr>
2013-05-10 19:18:15 +0900

    1. zoomin / zoomout rate fix. 	modified:   InfoBar.py

--- lib/python/Screens/InfoBar.py
+++ lib/python/Screens/InfoBar.py
@@ -181,7 +181,7 @@ class InfoBar(InfoBarBase, InfoBarShowHide,
 # [iq
 	def ZoomInOut(self):
 		zoomval=0
-		if self.zoomrate > 1:
+		if self.zoomrate > 0:
 			self.zoomin = 0
 		elif self.zoomrate < -9:
 			self.zoomin = 1
